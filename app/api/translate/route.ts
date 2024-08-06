3026424ae161b9a94fbef01d20a0e256f153bbee
Angel Leon <angel.leon@tekus.co>
2024-08-06 03:19:37 -0500

    linted

--- app/api/translate/route.ts
+++ app/api/translate/route.ts
@@ -27,29 +27,3 @@ export async function POST(req: Request) {
 		);
 	}
 }
-
-export default async function handler(
-	req: TranslateRequest,
-	res: TranslateResponse
-) {
-	if (req.method !== 'POST') {
-		return res
-			.status(405)
-			.json({ message: 'Only POST requests allowed' });
-	}
-
-	const { textForTranslate } = req.body;
-
-	try {
-		const prompt = `como un traductor profesional con mucha experiencia traduce el siguiente texto
-		limita tu respuesta solo al resultado de la traduccion
-		este es el texto: ${textForTranslate}`;
-
-		const translatedText = await getAiResponse(prompt);
-		res.status(200).json({ translatedText });
-	} catch (error) {
-		res
-			.status(500)
-			.json({ message: 'Error translating text', error });
-	}
-}
