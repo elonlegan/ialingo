59ff7e987dc150c1990b947b2e391af83878c773
Angel Leon <angel.leon@tekus.co>
2024-08-06 00:39:15 -0500

    ready click to copy toast

--- components/clickToCopy/clickToCopy.tsx
+++ components/clickToCopy/clickToCopy.tsx
@@ -4,16 +4,23 @@ import {
 	TooltipContent,
 	TooltipTrigger,
 } from '@/components/ui/tooltip';
+import { useToast } from '@/components/ui/use-toast';
 
 export default function ClickToCopy({ children, value }) {
+	const { toast } = useToast();
+
+	const handleCopy = () => {
+		navigator.clipboard.writeText(value);
+		toast({
+			title: 'Copiado!',
+			variant: 'success',
+		});
+	};
+
 	return (
 		<TooltipProvider>
 			<Tooltip>
-				<TooltipTrigger
-					onClick={() => {
-						navigator.clipboard.writeText(value);
-					}}
-				>
+				<TooltipTrigger onClick={handleCopy}>
 					{children}
 				</TooltipTrigger>
 				<TooltipContent>
